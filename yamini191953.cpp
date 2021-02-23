616e40cd4cf4bad08faacf43931524f148dae41c
Yamini2001 <60056299+Yamini2001@users.noreply.github.com>
2021-02-23 23:43:56 +0530

    ReadDisplayMatrix

--- yamini191953.cpp
+++ yamini191953.cpp
@@ -1,28 +1,41 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
-void getarray(int a[10][10],int x,int y);
+class matrix{
+	int a;
+	int b;
+	int A[3][3];
+	public:
+	  void readmatrix();
+	  void displaymatrix();
+};
+void matrix::readmatrix()
+{
+	for(int i=1;i<=3;i++)
+	{
+	  for(int j=1;j<=3;j++)
+	  {
+	    cout<<"A["<<i<<"]["<<j<<"] : ";
+	    cin>>A[i][j];
+	  }
+	}
+      }
+void matrix::displaymatrix()
+{
+   for(int i=1;i<=3;i++)
+   {
+     for(int j=1;j<=3;j++)
+     {
+	cout<<" "<<A[i][j];
+     }
+     cout<<endl;
+   }
+}
 int main()
 {
-	int a[10][10],m,n,i,j;
-	cout<<"Enter the value of m and n:";
-	cin>>m>>n;
-	getarray(a,m,n);
-	cout<<endl;
-	cout<<"You have entered following matrix"<<endl;
-	for(i=0;i<m;i++){
-		for(j=0;j<n;j++){
-			cout<<a[i][j]<<"\t";
-		}
-		cout<<endl;
-	}
-	return 0;
-}
-void getarray(int a[10][10],int x,int y)
-{
-	int i,j;
-	for(i=0;i<x;i++){
-		for(j=0;j<y;j++){
-			cin>>a[i][j];
-		}
-	}
+    matrix m;
+    m.readmatrix();
+    m.displaymatrix();
+    getch();
+    return 0;
 }
